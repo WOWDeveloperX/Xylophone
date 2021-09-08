39f624a71d33e8435d82bf281d346036105a0a58
WOWDeveloperX <wowdeveloperx@gmail.com>
2021-09-08 20:33:50 +0300

    buildKey();

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,19 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
+  void buildKey() {
+    Container(
+      color: Colors.red,
+      height: 120,
+      child: TextButton(
+        onPressed: () {
+          playSound(1);
+        },
+        child: null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,77 +31,17 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Container(
-                  color: Colors.red,
-                  height: 120,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(1);
-                    },
-                    child: null,
-                  ),
-                ),
-                Container(
-                  color: Colors.orange,
-                  height: 120,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(2);
-                    },
-                    child: null,
-                  ),
-                ),
-                Container(
-                  color: Colors.yellow,
-                  height: 120,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(3);
-                    },
-                    child: null,
-                  ),
-                ),
-                Container(
-                  color: Colors.green,
-                  height: 120,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(4);
-                    },
-                    child: null,
-                  ),
-                ),
-                Container(
-                  color: Colors.cyan,
-                  height: 120,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(5);
-                    },
-                    child: null,
-                  ),
-                ),
-                Container(
-                  color: Colors.blue,
-                  height: 120,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(6);
-                    },
-                    child: null,
-                  ),
-                ),
-                Container(
-                  color: Colors.deepPurple,
-                  height: 120,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(7);
-                    },
-                    child: null,
-                  ),
-                ),
-              ]),
+               buildKey();
+                buildKey();
+                 buildKey();
+                  buildKey();
+                   buildKey();
+                    buildKey();
+                     buildKey();
+
+
+              ]
+              ),
         ),
       ),
     );
