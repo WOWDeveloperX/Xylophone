c07071c0aeda789538c26d6bf52c5cb2e023be8e
WOWDeveloperX <wowdeveloperx@gmail.com>
2021-09-08 16:35:27 +0300

    flutButton==textButton

--- lib/main.dart
+++ lib/main.dart
@@ -13,68 +13,82 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(children: <Widget>[
-            Container(
-              color: Colors.red,
-              height: 60,
-              width: 200,
-              child: TextButton(
-                onPressed: () {
-                  playSound(1);
-                },
-                child: null,
-              ),
-            ),
-            Container(
-              color: Colors.orange,
-              child: TextButton(
-                onPressed: () {
-                  playSound(2);
-                },
-              ),
-            ),
-            Container(
-              color: Colors.yellow,
-              child: TextButton(
-                onPressed: () {
-                  playSound(3);
-                },
-              ),
-            ),
-            Container(
-              color: Colors.green,
-              child: TextButton(
-                onPressed: () {
-                  playSound(4);
-                },
-              ),
-            ),
-            Container(
-              color: Colors.cyan,
-              child: TextButton(
-                onPressed: () {
-                  playSound(5);
-                },
-              ),
-            ),
-            Container(
-              color: Colors.blue,
-              child: TextButton(
-                onPressed: () {
-                  playSound(6);
-                },
-              ),
-            ),
-            Container(
-              color: Colors.deepPurple,
-              child: TextButton(
-                onPressed: () {
-                  playSound(7);
-                },
-              ),
-            ),
-          ]),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  height: 120,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(1);
+                    },
+                    child: null,
+                  ),
+                ),
+                Container(
+                  color: Colors.orange,
+                  height: 120,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(2);
+                    },
+                    child: null,
+                  ),
+                ),
+                Container(
+                  color: Colors.yellow,
+                  height: 120,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(3);
+                    },
+                    child: null,
+                  ),
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 120,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(4);
+                    },
+                    child: null,
+                  ),
+                ),
+                Container(
+                  color: Colors.cyan,
+                  height: 120,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(5);
+                    },
+                    child: null,
+                  ),
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 120,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(6);
+                    },
+                    child: null,
+                  ),
+                ),
+                Container(
+                  color: Colors.deepPurple,
+                  height: 120,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(7);
+                    },
+                    child: null,
+                  ),
+                ),
+              ]),
         ),
       ),
     );
