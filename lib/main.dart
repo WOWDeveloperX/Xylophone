d6672b4ab2b78ccd914a8abc1d9e7c6f619ea712
WOWDeveloperX <wowdeveloperx@gmail.com>
2021-09-12 18:01:19 +0300

    Xulophone WAW FONT

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,18 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
-  Container buildKey({Color color, int soundNumber}) {
-    return Container(
-      color :color,
-      height: 120,
-      child: TextButton(
-        onPressed: () {
-          playSound(soundNumber);
-        },
-        child: null,
+  Padding buildKey({Color color, int soundNumber}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        color: color,
+        height: 66,
+        child: TextButton(
+          onPressed: () {
+            playSound(soundNumber);
+          },
+          child: null,
+        ),
       ),
     );
   }
@@ -27,18 +30,30 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('* * X y l o p h o n e * *',
+              style: TextStyle(
+                  fontFamily: 'Qwigley',
+                  fontSize: 55,
+                  color: Colors.red.shade100)),
+          backgroundColor: Colors.black,
+        ),
         body: SafeArea(
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                buildKey(color: Colors.red, soundNumber: 1),
-                buildKey(color: Colors.orange, soundNumber: 2),
-                buildKey(color: Colors.yellow, soundNumber: 3),
-                buildKey(color: Colors.green, soundNumber: 4),
-                buildKey(color: Colors.teal, soundNumber: 5),
-                buildKey(color: Colors.blue, soundNumber: 6),
-                buildKey(color: Colors.deepPurple, soundNumber: 7),
-              ]),
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(),
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.grey, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.deepPurple, soundNumber: 7),
+              buildKey(color: Colors.green, soundNumber: 8),
+              buildKey(color: Colors.white, soundNumber: 9),
+            ],
+          ),
         ),
       ),
     );
