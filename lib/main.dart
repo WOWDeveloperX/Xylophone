e86ca558ee99e307190bd62a8560b82fe9ee64fb
WOWDeveloperX <wowdeveloperx@gmail.com>
2021-09-20 19:29:27 +0300

    xylophone minimalism

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,12 @@ class XylophoneApp extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Container(
         color: color,
-        height: 66,
+        height: 62,
         child: TextButton(
           onPressed: () {
             playSound(soundNumber);
           },
-          child: null,
+          child: (Image.asset('images/button1.png')),
         ),
       ),
     );
@@ -31,10 +31,10 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-          title: Text('* * X y l o p h o n e * *',
+          title: Text(' - - X  y l o p h o~n e - -',
               style: TextStyle(
-                  fontFamily: 'Qwigley',
-                  fontSize: 55,
+                  fontFamily: 'CHERISH',
+                  fontSize: 54,
                   color: Colors.red.shade100)),
           backgroundColor: Colors.black,
         ),
@@ -42,16 +42,17 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(),
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 3),
-              buildKey(color: Colors.grey, soundNumber: 4),
-              buildKey(color: Colors.teal, soundNumber: 5),
-              buildKey(color: Colors.blue, soundNumber: 6),
-              buildKey(color: Colors.deepPurple, soundNumber: 7),
-              buildKey(color: Colors.green, soundNumber: 8),
-              buildKey(color: Colors.white, soundNumber: 9),
+              SizedBox(height: 5.0),
+              buildKey(color: Colors.black, soundNumber: 1),
+              buildKey(color: Colors.black, soundNumber: 2),
+              buildKey(color: Colors.black, soundNumber: 3),
+              buildKey(color: Colors.black, soundNumber: 4),
+              buildKey(color: Colors.black, soundNumber: 5),
+              buildKey(color: Colors.black, soundNumber: 6),
+              buildKey(color: Colors.black, soundNumber: 7),
+              buildKey(color: Colors.black, soundNumber: 8),
+              buildKey(color: Colors.black, soundNumber: 9),
+              buildKey(color: Colors.black, soundNumber: 10),
             ],
           ),
         ),
